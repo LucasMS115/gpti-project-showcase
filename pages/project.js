54809cb5d5d36b762387a699cdd53fa9442fcf5a
Victor Lopes <thevictorfl@gmail.com>
2021-10-03 00:48:10 -0300

    finishes about project page

--- pages/project.js
+++ pages/project.js
@@ -2,7 +2,8 @@ import styled from 'styled-components'
 import Meta from '../src/infra/Meta'
 import Working from '../src/components/Working'
 import Separator from '../src/components/Separator'
-import pinguins from '../public/images/pinguins.jpg'
+import eventInfo from '../public/images/about/event_info.png'
+import event from '../public/images/about/event.png'
 
 export default function Project() {
 
@@ -10,8 +11,7 @@ export default function Project() {
     <>
       <Meta title='O Projeto' />
       <TextContainer>
-        <h2>Frase sobre o objetivo do projeto</h2>
-        <p>Texto que descreve um pouco sobre a ideia principal do app, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <h2>Um novo meio para descobrir novas pessoas com interesses mútuos e redescobrir a cidade de São Paulo com o que ela tem de melhor para oferecer.</h2>
       </TextContainer>
 
       <Separator />
@@ -21,24 +21,24 @@ export default function Project() {
         <div>
           <div>
             <figure>
-              <img src={pinguins} alt="Pinguins" />
+              <img src={event} alt="Evento" />
             </figure>
           </div>
 
           <div>
-            <p>Texto que descreve um pouco de como o app funciona, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniamsed quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p>Um app mobile cujo objetivo consiste em reunir pessoas através de encontros relacionados a pontos turísticos e culturais na cidade de São Paulo, uma medida para promover a socialização de pessoas interessadas em encontros pessoais realizados nesses ambientes, bem como ressaltar a história e as atrações da cidade.</p>
           </div>
         </div>
 
         <div>
 
           <div>
-            <p>Texto que descreve um pouco de como o app funciona, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniamsed quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p>Diante do cenário pós-pandemia, a aplicação pode satisfazer o anseio de muitos pelo retorno de atividades ao ar livre, em pontos que até então estavam restritos pelas normas sanitárias em vigor.</p>
           </div>
 
           <div>
             <figure>
-              <img src={pinguins} alt="Pinguins" />
+              <img src={eventInfo} alt="Informações sobre o evento" />
             </figure>
           </div>
 
@@ -50,7 +50,7 @@ export default function Project() {
 
       <TextContainer>
 
-        <p>Texto que descreve um pouco sobre a ideia principal do app, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <p>O aplicativo se propõe como um meio para descobrir novas pessoas com interesses mútuos e redescobrir a cidade de São Paulo com o que ela tem de melhor para oferecer.</p>
 
       </TextContainer>
 
@@ -108,7 +108,7 @@ const SectionScreens = styled.section`
     img{
       width: 100%;
       height: 100%;
-      object-fit: cover;
+      object-fit: contain;
       border-radius: 8px;
       box-shadow: var(--default-shadow);
     }
